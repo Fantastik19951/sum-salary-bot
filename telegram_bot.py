2f5745aacbd2391aaf8d36a592d1fe8c5b8fc140
Fantastik19951 <uzya5kyll@gmail.com>
2025-05-15 01:26:39 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -219,6 +219,7 @@ async def show_main(msg, ctx, push=True):
     {SEPARATOR}
     
     📅 Текущий месяц: {MONTH_NAMES[today.month-1].capitalize()}
+    📅 Текущая дата : {dt.date.today()}
     💰 Суммарный оборот: {fmt_amount(month_total)} $
     📈 Заработок на сегодня: {fmt_amount(month_total * 0.1)} $
     """
